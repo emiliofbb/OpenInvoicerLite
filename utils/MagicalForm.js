4b77c8416b134ca5a9f2b18b4b1fedfcce735384
Emilio Fajin Bargo <emiliofbb@gmail.com>
2023-08-20 20:15:35 +0200

    started with the form for the models

---
+++ utils/MagicalForm.js
@@ -0,0 +1,11 @@
+function setMagicalFormData(magicalFormElement, object) {
+    
+}
+
+function getMagicalFormData(magicalFormElement) {
+    let result;
+    const childs = magicalFormElement.children;
+    for (let el in childs) {
+
+    }
+}
